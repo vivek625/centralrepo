3cd714f71b4cce5ada862b47adeda0d76c00de04
vivek625 <vivekrajsingh9006034@gmail.com>
2022-07-08 11:35:14 +0530

    house pattern

---
+++ house.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    for (int i = 1; i <= 10; i++)
+    {
+        for (int j = 1; j <=10; j++)
+        {
+            if (j==4 || j==10 || i==10 || i==4  || i-1==0 || i+j==4 )
+            
+            {
+                cout << ".";
+            }
+            else
+            {
+                cout <<" ";
+            }
+        }
+        cout<<endl;
+    }
+      
+    return 0;
+}
+
